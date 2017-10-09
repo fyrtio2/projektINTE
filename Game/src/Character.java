5215caaab27e396800bc8e2ac84f62ad6a4f8ed7
emilrosell <emro9957@student.su.se>
2017-10-09 09:54:13 +0200

    lägger till spelet

---
+++ Game/src/Character.java
@@ -0,0 +1,21 @@
+public class Character {
+
+    private int speed;
+    private double hp;
+
+
+    public Character(double hp, int speed){
+        this.hp = hp;
+        this.speed = speed;
+    }
+
+    public double getHp() {
+        return hp;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+
+}
---
+++ Game/tests/CharacterTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CharacterTest {
+    @Test
+    public void getHp() throws Exception {
+    }
+
+    @Test
+    public void getSpeed() throws Exception {
+    }
+
+}
