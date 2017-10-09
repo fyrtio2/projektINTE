bb23ee190ce8d0d7a137c6769c44b49284d25318
emilrosell <emro9957@student.su.se>
2017-10-09 11:13:43 +0200

    la till map testfall

---
+++ 02 Game/tests/MapTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapTest {
+    @Test
+    public void getLocationName() throws Exception {
+    }
+
+    @Test
+    public void getMapHeight() throws Exception {
+    }
+
+    @Test
+    public void getMapWidth() throws Exception {
+    }
+
+}
