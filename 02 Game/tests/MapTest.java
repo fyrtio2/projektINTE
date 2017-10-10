363913c066f84fd399fb253ed76744f00464b4c0
emilrosell <emro9957@student.su.se>
2017-10-10 11:44:22 +0200

    Testfall till Map

--- 02 Game/tests/MapTest.java
+++ 02 Game/tests/MapTest.java
@@ -9,13 +9,13 @@ public class MapTest {
         assertEquals("The forbidden forest", theWoods.getLocationName());
     }
 
-    @Test(expected = AssertionError.class) //Skall ej gå igenom förrän felhantering i klassen är ordnat
+    @Test(expected = IllegalArgumentException.class) //Skall ej gå igenom förrän felhantering i klassen är ordnat
     public void testNullLocationName(){
         Map nullMap = new Map(null, 500, 500);
         assertEquals(null, nullMap.getLocationName());
     }
 
-    @Test(expected = AssertionError.class) //empty string skall inte accepteras
+    @Test(expected = IllegalArgumentException.class) //empty string skall inte accepteras
     public void testEmptyString(){
         Map emptyStringMap = new Map("", 500, 500);
         assertEquals("", emptyStringMap.getLocationName());
@@ -28,5 +28,4 @@ public class MapTest {
     @Test
     public void getMapWidth() throws Exception {
     }
-
 }
