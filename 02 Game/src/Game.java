aec8c2245e22b8e4e7edb2ced379940b0f8234b8
emilrosell <emro9957@student.su.se>
2017-10-10 10:12:53 +0200

    game

--- 02 Game/src/Game.java
+++ 02 Game/src/Game.java
@@ -1,6 +1,30 @@
-public class Game {
+import javax.swing.*;
+
+public class Game extends JFrame{
+
+    public Game(){
+
+        JFrame frame = new JFrame();
+
+        setSize(600, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
+
 
     public static void main(String[] args) {
+        new Game();
+
+        Character mainCharacter = new Character(10, 5, 5,"DragonSlayer");
+        Map world = new Map("Emils world", 3,4);
+
+
+        System.out.println(mainCharacter.getName());
+        System.out.println(world.getLocationName());
+
+
 
     }
 }
