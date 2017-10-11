f4e278bed0575e3f0a4c739f074ea3946ad00c66
emilrosell <emro9957@student.su.se>
2017-10-11 10:36:40 +0200

    commit classes

--- 02 Game/tests/AllTests.java
+++ 02 Game/tests/AllTests.java
@@ -1,5 +1,15 @@
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value = Suite.class)
+@SuiteClasses(value = {MapTest.class})
+
+
 public class AllTests {
+
 
 }
