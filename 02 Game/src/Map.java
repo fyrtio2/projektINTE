4e7b3240ea601d0feb39b30299836d42b7b8b89b
emilrosell <emro9957@student.su.se>
2017-10-12 14:09:02 +0200

    la till dokumentation och annat skit

--- 02 Game/src/Map.java
+++ 02 Game/src/Map.java
@@ -1,21 +1,21 @@
 public class Map {
 
-    private String locationName;
+    private String mapName;
     private int mapHeight, mapWidth;
     private int mapMaxHeight = 1000;
     private int mapMaxWidth = 1000;
 
-    public Map(String locationName, int mapHeight, int mapWidth) {
-        this.locationName = locationName;
-        if (mapHeight < 0 || mapWidth < 0 || locationName == null || locationName.isEmpty()) {
+    public Map(String mapName, int mapHeight, int mapWidth) {
+        this.mapName = mapName;
+        if (mapHeight < 0 || mapWidth < 0 || mapName == null || mapName.isEmpty()) {
             throw new IllegalArgumentException("Something went wrong");
         }
         this.mapHeight = mapHeight > mapMaxHeight ? this.mapHeight = mapMaxHeight : mapHeight;
         this.mapWidth = mapWidth > mapMaxWidth ? this.mapWidth = mapMaxWidth : mapWidth;
     }
 
-    public String getLocationName() {
-        return locationName;
+    public String getMapName() {
+        return mapName;
     }
 
     public int getMapHeight() {
