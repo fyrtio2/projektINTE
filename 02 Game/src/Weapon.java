d8e59bf6c2b85882ed47ec8c63cdc14962d741d9
rogr1780 <robertgrubesic@hotmail.com>
2017-10-12 11:52:36 +0200

    Weapon class skapad

---
+++ 02 Game/src/Weapon.java
@@ -0,0 +1,10 @@
+public class Weapon extends Item {
+
+    WeaponAttributes weaponAttributes;
+
+    public Weapon(String name,int value,int weight,int durability, WeaponAttributes weaponAttributes){
+        super(name, value, weight, durability);
+
+
+    }
+}
