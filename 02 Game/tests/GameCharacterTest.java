475df22dc133f004edb14742b20b19a40513495d
Peter Yakob <32325216+Peter-Yakob@users.noreply.github.com>
2017-10-12 14:44:08 +0200

    Merger branches

--- 02 Game/tests/GameCharacterTest.java
+++ 02 Game/tests/GameCharacterTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class GameCharacterTest {
+    //
     //HP Tests
     @Test
     public void testStartHpTest() throws Exception {
