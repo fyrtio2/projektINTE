70da854623415b9f6ed76cad620f801a41ee489d
emilrosell <emro9957@student.su.se>
2017-10-12 14:25:45 +0200

    weapon class

--- 02 Game/src/Weapon.java
+++ 02 Game/src/Weapon.java
@@ -1,10 +1,38 @@
 public class Weapon extends Item {
 
     WeaponAttributes weaponAttributes;
+    private boolean isBroken;
+    private int lvlReq;
 
-    public Weapon(String name,int value,int weight,int durability, WeaponAttributes weaponAttributes){
+    public Weapon(String name,int value,int weight,int durability, WeaponAttributes weaponAttributes, int lvlReq){
         super(name, value, weight, durability);
+        this.lvlReq = lvlReq;
 
 
+    }
+
+    public boolean isWearableBy(GameCharacter player) {
+        if (isBroken) {
+            return false;
+        }
+        if (player.getLevel() >= lvlReq) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void damageEquipment(int damage) {
+        int durability = getDurability();
+        durability -= damage;
+        setDurability(durability);
+        if (durability <= 0) {
+            isBroken = true;
+        }
+    }
+
+    public boolean isBroken() {
+        return isBroken;
     }
 }
