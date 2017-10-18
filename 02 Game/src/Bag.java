9acf8c3f8ef4c2a58291b14c28c6aa7d5ec6ed67
emilrosell <emro9957@student.su.se>
2017-10-18 14:00:08 +0200

    testfall testfall

--- 02 Game/src/Bag.java
+++ 02 Game/src/Bag.java
@@ -1,23 +1,35 @@
 import java.util.HashMap;
 import java.util.HashSet;
-// comment
+
 public class Bag {
-    private int maxWeight = 100;
+
+    private int weight;
     private HashMap<String, Item> bag = new HashMap<>();
 
-    public Bag(int size) {
-        this.maxWeight = size;
+
+    public Bag(int weight) {
+        this.weight = weight;
     }
 
     public void addToBag(Item item) {
         bag.put(item.getName(), item);
+        addWeight(item);
     }
-
     public HashMap getHashMap() {
         return bag;
     }
 
-    public void removeFromBag(Item item) {
-        bag.remove(item.getName(), item);
+    public void removeFromBag(Item item){
+        bag.remove(item.getName(),item);
     }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void addWeight(Item item) {
+        weight += item.getWeight();
+    }
+
+
 }
