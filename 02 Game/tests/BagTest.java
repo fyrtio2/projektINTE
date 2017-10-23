7de151d4193b92007a1e95900c20dddecf31a4d6
emilrosell <emro9957@student.su.se>
2017-10-23 13:38:03 +0200

    pptx

--- 02 Game/tests/BagTest.java
+++ 02 Game/tests/BagTest.java
@@ -20,12 +20,11 @@ public class BagTest {
     public void testWeight() {
         Bag bag = new Bag(10);
         EquipmentAttributes attributes = new EquipmentAttributes(5, 5, 5, 5, 5);
-
-        Equipment ring = new Equipment(Equipment.Type.jewelry, "Ring", 2, 10, 10, attributes); // Creates equipment with level 2 requirement
+        Equipment ring = new Equipment(Equipment.Type.jewelry, "Ring", 2, 10, 10, attributes);      // Creates equipment with level 2 requirement
         bag.addToBag(ring);
-
         assertEquals(20, bag.getWeight());
     }
+
 
     @Test(expected = AssertionError.class)
     public void testRemoveFromBag() {
