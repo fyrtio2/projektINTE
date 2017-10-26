813d0974aabb25998b74fc55f802606722c2002f
rogr1780 <robertgrubesic@hotmail.com>
2017-10-26 10:06:39 +0200

    pickUpPerformanceTest

--- 02 Game/src/Game.java
+++ 02 Game/src/Game.java
@@ -1,22 +1,35 @@
 import java.util.Random;
+import java.util.UUID;
+import java.util.HashMap;
+
 
 public class Game {
+    private GameCharacter dragonSlayer;
     public static void main(String[] args) {
-        GameCharacter dragonSlayer = new GameCharacter("DragonSlayer");
 
-        char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        StringBuilder sb = new StringBuilder();
-        Random random = new Random();
+        Game run = new Game();
 
-        for (int i = 0; i < 2000; i++) {
-            char c = chars[random.nextInt(chars.length)];
-            sb.append(c);
-        }
-        String output = sb.toString();
 
-        for(int i=0; i<10000000; i++){
+        run.pickUpItemsPerformanceTest(1000000);
+
+
+    }
+
+
+    private void pickUpItemsPerformanceTest(int pickUpIterations){
+
+
+
+
+        for(int i=0; i<pickUpIterations; i++){
+            String uniqueID = UUID.randomUUID().toString();
+            dragonSlayer = new GameCharacter("DragonSlayer");
             EquipmentAttributes equipmentAttributes = new EquipmentAttributes(10, 10, 10, 10, 10);
-            Equipment equipment = new Equipment(Equipment.Type.shoes,output, 10, 10, 10,equipmentAttributes);
+            Equipment equipment = new Equipment(Equipment.Type.shoes,uniqueID, 10, 10, 10,equipmentAttributes);
+
+
+
+
             dragonSlayer.pickUpItem(equipment);
 
         }
